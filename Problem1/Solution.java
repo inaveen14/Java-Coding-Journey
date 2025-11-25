34d9114861904c4e885831e944d62952e1d62a65
Naveen Kumar <115611478+inaveen14@users.noreply.github.com>
2025-11-25 16:24:28 +0530

    Create Solution.java

---
+++ Problem1/Solution.java
@@ -0,0 +1,19 @@
+public class Javadayelevenone {
+    public static void drawPattern(int row, int col) { // Changed to 'void'
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (i == 1 || i == row || j == 1 || j == col) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        // No return needed now!
+    }
+
+    public static void main(String[] args) {
+        drawPattern(4, 5); // No System.out.println here!
+    }
+}
